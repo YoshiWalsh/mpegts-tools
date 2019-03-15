0d4c147109ff5561d9541a1bac19697260dab696
Joshua Walsh <josh@ymindustries.com>
2019-03-15 22:19:21 +1100

    Modify PacketAligner to output more than one packet at a time
    This should improve performance in certain scenarios
    
    Also improved the handling of a loss-of-sync situation, now it doesn't wait until new data is received before attempting to regain sync

--- src/PacketAligner.ts
+++ src/PacketAligner.ts
@@ -17,10 +17,11 @@ export class PacketAligner extends Transform {
 	public _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
 		try {
 			this.buffer = Buffer.concat([this.buffer, chunk]);
-			if(!this.syncAcquired) {
-				const syncByteIndex = this.buffer.indexOf(MPEGTS_SYNC_BYTE, 0);
-				this.buffer = this.buffer.slice(syncByteIndex >= 0 ? syncByteIndex : this.buffer.length);
-				while(this.buffer.length > MPEGTS_PACKET_LENGTH * 2 + 1) {
+			let output = Buffer.alloc(0);
+			while (this.buffer.length > (this.syncAcquired ? MPEGTS_PACKET_LENGTH : (MPEGTS_PACKET_LENGTH * 2 + 1))) {
+				if(!this.syncAcquired) {
+					const syncByteIndex = this.buffer.indexOf(MPEGTS_SYNC_BYTE, 0);
+					this.buffer = this.buffer.slice(syncByteIndex >= 0 ? syncByteIndex : this.buffer.length);
 					// Search for 3 properly-spaced sync bytes in a row
 					if(
 						this.buffer[MPEGTS_PACKET_LENGTH * 0] === MPEGTS_SYNC_BYTE &&
@@ -28,23 +29,22 @@ export class PacketAligner extends Transform {
 						this.buffer[MPEGTS_PACKET_LENGTH * 2] === MPEGTS_SYNC_BYTE
 					) {
 						this.syncAcquired = true;
-						break;
+						continue;
 					}
 					
-					const syncByteIndex = this.buffer.indexOf(MPEGTS_SYNC_BYTE, 1);
-					this.buffer = this.buffer.slice(syncByteIndex >= 0 ? syncByteIndex : this.buffer.length);
-				}
-			}
-			if(this.syncAcquired) {
-				while(this.buffer.length > MPEGTS_PACKET_LENGTH) {
+					this.buffer = this.buffer.slice(1);
+				} else {
 					if(this.buffer[MPEGTS_PACKET_LENGTH] === MPEGTS_SYNC_BYTE) {
-						this.push(this.buffer.slice(0, MPEGTS_PACKET_LENGTH));
+						output = Buffer.concat([output, this.buffer.slice(0, MPEGTS_PACKET_LENGTH)]);
 						this.buffer = this.buffer.slice(MPEGTS_PACKET_LENGTH);
 					} else {
 						this.syncAcquired = false;
-						break;
+						continue;
 					}
 				}
+			}
+			if(output.length > 0) {
+				this.push(output);
 			}
 			callback(null);
 		} catch (ex) {
