e787dc4bb28315b87695ffe694f45f87b4f3dec6
Joshua Walsh <josh@ymindustries.com>
2019-04-12 03:05:29 +1000

    Improve PacketAligner performance with large input chunk sizes
    Reduces the need for allocations (via Buffer.concat) in PacketAligner, resulting in significantly better performance when input data contains larger chunks.
    
    **Benchmark results for this change:**
    **128B**
    Before: Processed 536870912 Bytes in 203.38763859999924 seconds. (avg. 2.5173604626333566 MiB/s)
    After: Processed 536870912 Bytes in 213.39299560000003 seconds. (avg. 2.3993289871600636 MiB/s)
    **512B**
    Before: Processed 536870912 Bytes in 52.33426679900288 seconds. (avg. 9.783264987095512 MiB/s)
    After: Processed 536870912 Bytes in 50.607413599997756 seconds. (avg. 10.117094780754073 MiB/s)
    **1KB**
    Before: Processed 536870912 Bytes in 27.883386900000275 seconds. (avg. 18.362188274911286 MiB/s)
    After: Processed 536870912 Bytes in 26.72388340000063 seconds. (avg. 19.158892154124125 MiB/s)
    **8KB**
    Before: Processed 536870912 Bytes in 8.657928899995982 seconds. (avg. 59.13654476883468 MiB/s)
    After: Processed 536870912 Bytes in 4.350168901003897 seconds. (avg. 117.6965795240375 MiB/s)
    **16KB**
    Before: Processed 536870912 Bytes in 12.300546700000764 seconds. (avg. 41.62416618441587 MiB/s)
    After: Processed 536870912 Bytes in 7.173161199994385 seconds. (avg. 71.37717747098738 MiB/s)

--- src/PacketAligner.ts
+++ src/PacketAligner.ts
@@ -1,5 +1,6 @@
 import { Transform, TransformCallback } from 'stream';
 import { MPEGTS_PACKET_LENGTH, MPEGTS_SYNC_BYTE } from './Packets';
+import { timingSafeEqual } from 'crypto';
 
 /**
  * This transformer takes an MPEG Transport Stream and aligns it so that each chunk will always start at the beginning of a packet, and will contain an integer amount of packets.
@@ -7,44 +8,117 @@ import { MPEGTS_PACKET_LENGTH, MPEGTS_SYNC_BYTE } from './Packets';
  * This makes it simple to parse and perform operations on the packets.
  */
 export class PacketAligner extends Transform {
-	private buffer: Buffer = Buffer.alloc(0, undefined);
+	private leftoversMaxLength = MPEGTS_PACKET_LENGTH * 3;
+	private leftovers: Buffer = Buffer.alloc(this.leftoversMaxLength, undefined);
+	private leftoversEnd = 0;
+	private temp: Buffer = Buffer.alloc(this.leftoversMaxLength, undefined); // This is used as temporary storage for the leftovers value in case we need to shift it
+	private emptyBuffer = Buffer.alloc(0);
 	private syncAcquired = false;
 
 	public constructor() {
 		super();
 	}
 
+	private indexOfLeftoversAndChunk(chunk: Buffer, value: number, byteOffset: number): number {
+		const positionWithinLeftovers = this.leftovers.slice(0, this.leftoversEnd).indexOf(value, byteOffset);
+		if(positionWithinLeftovers !== -1) {
+			return positionWithinLeftovers;
+		}
+
+		const positionWithinChunk = chunk.indexOf(value, byteOffset + this.leftoversEnd);
+		if(positionWithinChunk !== -1) {
+			return positionWithinChunk + this.leftoversEnd;
+		}
+		return -1;
+	}
+
+	private getByteFromLeftoversAndChunk(chunk: Buffer, index: number): number {
+		if(index < this.leftoversEnd) {
+			return this.leftovers[index];
+		} else {
+			return chunk[index - this.leftoversEnd];
+		}
+	}
+
+	private shiftLeftovers(amount: number) {
+		if(amount >= this.leftoversEnd) {
+			this.leftoversEnd = 0;
+			return;
+		}
+		this.leftovers.slice(amount).copy(this.temp);
+		this.leftoversEnd -= amount;
+		this.temp.copy(this.leftovers, 0, 0, this.leftoversEnd);
+	}
+
+	private shiftLeftoversAndChunk(chunk: Buffer, amount: number): Buffer {
+		const amountToShiftChunk = Math.max(0, amount - this.leftoversEnd);
+
+		if(amountToShiftChunk >= chunk.length) {
+			chunk = this.emptyBuffer;
+		} else if (amountToShiftChunk > 0) {
+			chunk = chunk.slice(amountToShiftChunk);
+		}
+		
+		this.shiftLeftovers(amount);
+		return chunk;
+	}
+
+	private appendToLeftovers(chunk: Buffer, minimumShift?: number) {
+		if(chunk.length > this.leftoversMaxLength) {
+			chunk = chunk.slice(chunk.length - this.leftoversMaxLength);
+		}
+
+		const totalSize = this.leftoversEnd + chunk.length;
+		let shiftAmount = totalSize - this.leftoversMaxLength;
+		if(minimumShift && shiftAmount < minimumShift) {
+			shiftAmount = minimumShift;
+		}
+		if(shiftAmount > 0) {
+			this.shiftLeftovers(shiftAmount);
+		}
+		if(chunk.length > 0) {
+			chunk.copy(this.leftovers, this.leftoversEnd);
+		}
+		this.leftoversEnd += chunk.length;
+	}
+
 	public _transform(chunk: Buffer, encoding: BufferEncoding, callback: TransformCallback) {
 		try {
-			this.buffer = Buffer.concat([this.buffer, chunk]);
-			let output = Buffer.alloc(0);
-			while (this.buffer.length > (this.syncAcquired ? MPEGTS_PACKET_LENGTH : (MPEGTS_PACKET_LENGTH * 2 + 1))) {
+			let output = [];
+			while (this.leftoversEnd + chunk.length > (this.syncAcquired ? MPEGTS_PACKET_LENGTH : (MPEGTS_PACKET_LENGTH * 2 + 1))) {
 				if(!this.syncAcquired) {
-					const syncByteIndex = this.buffer.indexOf(MPEGTS_SYNC_BYTE, 0);
-					this.buffer = this.buffer.slice(syncByteIndex >= 0 ? syncByteIndex : this.buffer.length);
+					const syncByteIndex = this.indexOfLeftoversAndChunk(chunk, MPEGTS_SYNC_BYTE, 0);
 					// Search for 3 properly-spaced sync bytes in a row
 					if(
-						this.buffer[MPEGTS_PACKET_LENGTH * 0] === MPEGTS_SYNC_BYTE &&
-						this.buffer[MPEGTS_PACKET_LENGTH * 1] === MPEGTS_SYNC_BYTE &&
-						this.buffer[MPEGTS_PACKET_LENGTH * 2] === MPEGTS_SYNC_BYTE
+						this.getByteFromLeftoversAndChunk(chunk, syncByteIndex + MPEGTS_PACKET_LENGTH * 0) === MPEGTS_SYNC_BYTE &&
+						this.getByteFromLeftoversAndChunk(chunk, syncByteIndex + MPEGTS_PACKET_LENGTH * 1) === MPEGTS_SYNC_BYTE &&
+						this.getByteFromLeftoversAndChunk(chunk, syncByteIndex + MPEGTS_PACKET_LENGTH * 2) === MPEGTS_SYNC_BYTE
 					) {
+						chunk = this.shiftLeftoversAndChunk(chunk, syncByteIndex);
 						this.syncAcquired = true;
 						continue;
 					}
 					
-					this.buffer = this.buffer.slice(1);
+					this.appendToLeftovers(chunk, 1);
+					chunk = this.emptyBuffer;
 				} else {
-					if(this.buffer[MPEGTS_PACKET_LENGTH] === MPEGTS_SYNC_BYTE) {
-						output = Buffer.concat([output, this.buffer.slice(0, MPEGTS_PACKET_LENGTH)]);
-						this.buffer = this.buffer.slice(MPEGTS_PACKET_LENGTH);
+					if(this.getByteFromLeftoversAndChunk(chunk, MPEGTS_PACKET_LENGTH) === MPEGTS_SYNC_BYTE) {
+						if(this.leftoversEnd > 0) {
+							output.push(this.leftovers.slice(0, Math.min(this.leftoversEnd, MPEGTS_PACKET_LENGTH)));
+						}
+						if(MPEGTS_PACKET_LENGTH > this.leftoversEnd) {
+							output.push(chunk.slice(0, MPEGTS_PACKET_LENGTH - this.leftoversEnd));
+						}
+						chunk = this.shiftLeftoversAndChunk(chunk, MPEGTS_PACKET_LENGTH);
 					} else {
 						this.syncAcquired = false;
 						continue;
 					}
 				}
 			}
+			this.appendToLeftovers(chunk);
 			if(output.length > 0) {
-				this.push(output);
+				this.push(Buffer.concat(output));
 			}
 			callback(null);
 		} catch (ex) {
